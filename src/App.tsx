fbaaecb3310d12b670184117715ccd87bbcf839c
Karthik Sankaran <145926836+karsan377@users.noreply.github.com>
2026-09-25 14:10:37 -0700

    added head body and arms

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,33 @@
- import { useState } from 'react'
- import words from './wordList.json'
- 
- 
- function App (){
+import { useState } from 'react'
+import words from './wordList.json'
+import { HangmanDrawing } from './HangmanDrawing'
+
+
+function App() {
   const [wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
 
   const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  
   return (
-    <div 
-    style = {{
-    maxWidth: '800px',
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '2rem',
-    margin: '0 auto',
-    alignItems: 'center',
-    }}
-  >
-    <div style = {{ fontSize: '2rem', textAlign: 'center'}}> Lose Win</div>
-    
+    <div
+      style={{
+        maxWidth: '800px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem',
+        margin: '0 auto',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ fontSize: '2rem', textAlign: 'center' }}>Lose Win</div>
 
-    <HamgmanWord />
-    <HamgmanDrawing />
-    </Keyboard >
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
     </div>
   )
- 
+}
 
- export default App
-
- }
+export default App
