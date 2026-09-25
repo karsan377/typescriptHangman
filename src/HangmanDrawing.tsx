c7e6d3aeec1a5df0ab7d885282eb04229eaf4d3b
Karthik Sankaran <145926836+karsan377@users.noreply.github.com>
2026-09-24 18:54:48 -0700

    started Hangman Drawing

---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,6 @@
+export function HamgmanDrawing() {
+    return (
+        <div style = {{position: 'relative'}}>
+            <div style = {{height: '10px', width: 250px, background: "black"}}
+        <HamgmanDrawing
+}
---
+++ src/Keyboard.tsx
@@ -0,0 +1,2 @@
+export function Keyboard() {
+}
